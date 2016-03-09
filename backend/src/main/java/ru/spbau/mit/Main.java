c72812cb2d1cf2658355ca4664f40e248bc4aedc
ArgentumWalker <svidchen-ko@mail.ru>
2016-03-09 18:31:34 +0400

    Resources usage and GUI output

--- backend/src/main/java/ru/spbau/mit/Main.java
+++ backend/src/main/java/ru/spbau/mit/Main.java
@@ -4,20 +4,22 @@ import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 import static org.bytedeco.javacpp.opencv_objdetect.*;
+import static org.bytedeco.javacpp.opencv_highgui.*;
 
 public class Main{
 	
     static final String frontalfaceXmlPath = "haarcascade_frontalface_alt.xml";
     static final String inputImage = "input.jpg";
-    static final String outputImage = "output.jpg";
-    static final String filePathPrefix = "C:\\Users\\ArgentumWalker\\Desktop\\AuProject\\reptilian-detector\\backend\\src\\main\\resources\\";
     
     public static void main(String[] arg){
-        IplImage image = cvLoadImage(filePathPrefix+inputImage);
+        final ResourceMenager resources = new ResourceMenager();
+        System.out.println(resources.getPathTo(inputImage));
+        IplImage image = cvLoadImage(resources.getPathTo(inputImage));
         if(image != null){
             detect_faces(image);
-            cvSaveImage(filePathPrefix+outputImage,image);
-            cvReleaseImage(image);
+            namedWindow("", WINDOW_NORMAL);
+            cvShowImage("Result",image);
+            cvWaitKey(0);
             System.out.println("OK.");
         } else {
             System.out.println("Can't load image.");
@@ -25,15 +27,23 @@ public class Main{
     }
     
     static void detect_faces(IplImage image){
-        CvHaarClassifierCascade cascade = new CvHaarClassifierCascade(cvLoad(filePathPrefix+frontalfaceXmlPath));
+        final ResourceMenager resources = new ResourceMenager();
+        CvHaarClassifierCascade cascade = new CvHaarClassifierCascade(cvLoad(resources.getPathTo(frontalfaceXmlPath)));
         CvMemStorage storage = CvMemStorage.create();
         CvSeq faces = cvHaarDetectObjects(image, cascade, storage, 1.5, 3, CV_HAAR_DO_CANNY_PRUNING);
         cvClearMemStorage(storage);
         int totalCount = faces.total();
         for(int i = 0; i < totalCount; i++){
             CvRect rect = new CvRect(cvGetSeqElem(faces,i));
-            cvRectangle(image, cvPoint(rect.x(), rect.y(), cvPoint(rect.width() + rect.x(), rect.height() + rect.y()), CvScalar.GREEN, 4, CV_AA, 0);            
+            cvRectangle(image, cvPoint(rect.x(), rect.y()), cvPoint(rect.width() + rect.x(), rect.height() + rect.y()), CvScalar.GREEN, 4, CV_AA, 0);            
         }
     }
 	
+}
+
+class ResourceMenager{
+    public String getPathTo(String resourceName){
+        String path = getClass().getResource("/" + resourceName).getPath();
+        return path.substring(1, path.length());
+    }
 }
